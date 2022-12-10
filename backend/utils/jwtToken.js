9a74f4ae1c1841a5bb2120035a9835baa38995e5
Avinash <avinashsingh880086@gmail.com>
2022-12-10 21:34:43 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,7 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + (30*24*3600000)
         ),
-        httpOnly: true
+        httpOnly: false
     };
 
     res.status(statusCode).cookie('token', token, options).json({
